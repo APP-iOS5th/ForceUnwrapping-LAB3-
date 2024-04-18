f5df9f8c6ca057f6ed29f9999dedeb7f1e7d2d13
WonyeolHEO <hyy96hyy@naver.com>
2024-04-18 14:22:52 +0900

    There are many error

--- CalculatorApp/CalculatorApp/ContentView.swift
+++ CalculatorApp/CalculatorApp/ContentView.swift
@@ -8,16 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var inputNumber = ""
+    @State var calculatedNumber = 0.0
+    
+    let buttonName = ["7", "8", "9", "/", "4", "5", "6", "*", "1","2","3","-",".","0","C","+"]
+    let columns: [GridItem] = Array(repeating: GridItem(.flexible()), count: 4)
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(inputNumber)
+            LazyVGrid(columns: columns, spacing: 50) {
+                ForEach(0..<16, id: \.self) { index in
+                    Button(action: {
+                        if buttonName[index] == "C"{
+                            inputNumber = ""
+                        } else {
+                            inputNumber += buttonName[index]}
+                    }) {
+                        Text(buttonName[index])
+                    }
+                }
+            }
+            Button(action: {calculateResult()}) {
+                Text("계산하기")
+            }
+            Text("= \(calculatedNumber)")
+            
         }
-        .padding()
+    }
+    
+    func calculateResult() {
+        let expression = NSExpression(format: inputNumber)
+        if let result = expression.expressionValue(with: nil, context: nil) as? Double {
+            calculatedNumber = result
+        }
     }
 }
+
+
+
+
 
 #Preview {
     ContentView()
