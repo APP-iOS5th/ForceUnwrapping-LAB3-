d45a730983c073bcb3835fa597535f3b93dbe2ea
seokyung402 <wni402@naver.com>
2024-04-22 15:35:52 +0900

    Mememo

---
+++ Mememo/Mememo/MemoStore.swift
@@ -0,0 +1,39 @@
+//
+//  MemoStore.swift
+//  Mememo
+//
+//  Created by seokyung on 4/22/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct Memo: Identifiable {
+    var id: UUID = UUID()
+    var text: String
+    var color: Color
+    var created: Date
+    
+    var createdString: String {
+        get {
+            let dateFormatter: DateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            return dateFormatter.string(from: created)
+        }
+    }
+}
+
+class MemoStore: ObservableObject {
+    @Published var memos: [Memo] = []
+    
+    func addMemo(_ text: String, color: Color) {
+        let memo: Memo = Memo(text: text, color: color, created: Date())
+        memos.insert(memo, at: 0)
+    }
+    
+    func removeMemo(_ targetMemo: Memo) {
+        if let index = memos.firstIndex(where: { $0.id == targetMemo.id }) {
+            memos.remove(at: index)
+        }
+    }
+}
